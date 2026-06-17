dbd9a664ac06e388645182f67a92a5bf382fc1c1
SamFinkiel <samuelfinkiel@gmail.com>
2026-06-16 22:58:03 -0400

    "class 48 hw"

---
+++ html/48/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpAssesment {
+    internal class Program {
+        static void Main(string[] args) {
+            List<Shirt> shirts = new List<Shirt>();
+            string[] colors = new string[] { "red", "green", "blue" };
+            string[] patterns = new string[] { "stripped", "checked", "plain" };
+
+            for (int i = 0; i < colors.Length; i++) {
+                for (int j = 0; j < patterns.Length; j++) {
+                    shirts.Add(new Shirt(colors[i], patterns[j]));
+                }
+            }
+
+            foreach (Shirt shirt in shirts) {
+                Console.WriteLine($"{shirt.Color}: {shirt.Pattern}");
+            }
+        }
+    }
+}
---
+++ html/48/Shirt.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpAssesment {
+    public class Shirt {
+        public string Color { get; set; }
+        public string Pattern { get; set; }
+        public Shirt(string color, string pattern) {
+            Color = color;
+            Pattern = pattern;
+        }
+    }
+}
