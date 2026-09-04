55acf861006408a59dcd466527b4dfed2aa533c7
SamFinkiel <samuelfinkiel@gmail.com>
2026-09-03 22:54:44 -0400

    HW 62

--- html/HTML/48/Program.cs
+++ html/HTML/48/Program.cs
@@ -3,7 +3,7 @@
         static void Main(string[] args) {
             List<Shirt> shirts = new List<Shirt>();
             string[] colors = new string[] { "red", "green", "blue" };
-            string[] patterns = new string[] { "striped", "checked", "plain" };
+            string[] patterns = new string[] { "stripped", "checked", "plain" };
 
             for (int i = 0; i < colors.Length; i++) {
                 for (int j = 0; j < patterns.Length; j++) {
